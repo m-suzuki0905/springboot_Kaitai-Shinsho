429422b9956b29402480fac4497182feae214e57
Suzuki Masato <suzuki.masato.work@gmail.com>
2022-09-20 16:46:08 +0900

    バインドができなかったエラーを修正(formにmethodを付与),バリデーションのエラーメッセージを実装 #5

--- src/main/java/com/example/controller/SignupController.java
+++ src/main/java/com/example/controller/SignupController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,11 +34,12 @@ public class SignupController {
 
 		// ユーザー登録画面に遷移させる
 		return "user/signup";
-	}
+	};
 
 	/* ユーザーの登録処理 */
 	@PostMapping("/signup")
-	public String postSignup(Model model, Locale locale, @ModelAttribute SignupForm form, BindingResult bindingResult) {
+	public String postSignup(Model model, Locale locale, @ModelAttribute @Validated SignupForm form,
+			BindingResult bindingResult) {
 		// 入力チェック結果
 		if (bindingResult.hasErrors()) {
 			// NGを出してユーザー登録画面に戻す
@@ -48,5 +50,5 @@ public class SignupController {
 
 		// ログイン画面にリダイレクトする
 		return "redirect:/login";
-	}
+	};
 }
