f4b0efaaf17c642a25e257600ed9094a4124e00a
Suzuki Masato <masato05rock27@gmail.com>
2022-09-23 21:36:20 +0900

    update・delete処理ができるユーザー詳細画面の更新ボタンと削除ボタンを実装完了 #8

--- src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/domain/user/service/impl/UserServiceImpl.java
@@ -34,4 +34,17 @@ public class UserServiceImpl implements UserService {
 	public MUser getUserOne(String userId) {
 		return mapper.findOne(userId);
 	}
+
+	/* ユーザー更新 1件 */
+	@Override
+	public void updateUserOne(String userId, String password, String userName) {
+		// TODO 自動生成されたメソッド・スタブ
+		mapper.updateOne(userId, password, userName);
+	}
+
+	/* ユーザー削除 1件 */
+	@Override
+	public void deleteUserOne(String userId) {
+		int count = mapper.deleteOne(userId);
+	}
 }
