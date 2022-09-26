32737f559e877c35aa6a6532a5e87e3342012801
Suzuki Masato <suzuki.masato.work@gmail.com>
2022-09-26 13:59:08 +0900

    Aroundの実装 全コントローラーのメソッドに開始・終了ログ出力するAOPメソッドを実装 #14

--- src/main/java/com/example/aspect/LogAspect.java
+++ src/main/java/com/example/aspect/LogAspect.java
@@ -1,7 +1,9 @@
 package com.example.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -27,5 +29,33 @@ public class LogAspect {
 	@After("execution(* *..*.*UserService.*(..))")
 	public void endLog(JoinPoint jp) {
 		log.info("メソッド終了:" + jp.getSignature());
+	}
+
+	/*
+	 * コントローラー全ての実行前後にログを出力する 以下でbeanとアノテーションの直接指定もいける
+	 */
+	// @Around("bean(*Controller*)")
+	// @Around("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+	@Around("@within(org.springframework.stereotype.Controller)")
+	public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+		// 開始ログ出力
+		log.info("メソッド開始:" + jp.getSignature());
+
+		try {
+			// メソッド実行
+			Object result = jp.proceed();
+
+			// 終了ログ出力
+			log.info("メソッド終了:" + jp.proceed());
+
+			// 実行結果を呼び出し元に返却する
+			return result;
+		} catch (Exception e) {
+			// エラーログ出力
+			log.error("メソッド異常終了:" + jp.getSignature());
+
+			// エラーを再度スローする
+			throw e;
+		}
 	}
 }
